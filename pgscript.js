da476108cbc2b0d62197c0de84b6731b079236a6
inodonis <122876203+inodonis@users.noreply.github.com>
2024-01-01 08:32:11 +0300

    Add files via upload

--- pgscript.js
+++ pgscript.js
@@ -5,7 +5,26 @@ document.querySelectorAll('.add-to-cart').forEach(button => {
         const productPrice = parseFloat(this.parentElement.getAttribute('data-price'));
         totalPurchased += productPrice;
 
-        // Show notification
         alert(`You have purchased items worth Birr ${totalPurchased.toFixed(2)}`);
     });
 });
+
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();  // Prevent form submission
+    alert("Thank you for contacting us!");
+  });
